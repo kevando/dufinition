05178a61b4cb5c3befad5893be82d83a3541d3f5
kevinframeri <kevin@frameri.com>
2015-10-24 20:07:44 -0400

    tutorial code for the book search

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dufinition
 Upload a photo and define your world
+
+
+
+# read me ideas
+talk about react-native and how I accomplish my goals to access the dictionary and the camera
+similar to: http://tech.taskrabbit.com/blog/2015/09/21/react-native-example-app/
+
+https://github.com/facebook/react-native/tree/master/Examples/Movies
--- index.ios.js
+++ index.ios.js
@@ -1,56 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
-
+ 
 var React = require('react-native');
-
-
+var Featured = require('./Featured');
+var Search = require('./Search');
+ 
 var {
-  AppRegistry,
-  Image,
-  StyleSheet,
-  Text,
-  View,
-} = React;
-
-
-var MOCKED_MOVIES_DATA = [
-  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
-];
-
-
-var AwesomeProject = React.createClass({
-  render: function() {
-    var movie = MOCKED_MOVIES_DATA[0];
-    return (
-        <View style={styles.container}>
-          <Image
-            source={{uri: movie.posters.thumbnail}}
-            style={styles.thumbnail}
-          />
-          <View style={styles.rightContainer}>
-            <Text style={styles.title}>{movie.title}</Text>
-            <Text style={styles.year}>{movie.year}</Text>
-          </View>
-        </View>
-      );
-  }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-});
-
+    AppRegistry,
+    TabBarIOS,
+    Component
+   } = React;
+ 
+class AwesomeProject extends Component {
+ 
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 'featured'
+        };
+    }
+ 
+    render() {
+        return (
+            <TabBarIOS selectedTab={this.state.selectedTab}>
+                <TabBarIOS.Item
+                    selected={this.state.selectedTab === 'featured'}
+                    title='featured'
+                    onPress={() => {
+                        this.setState({
+                            selectedTab: 'featured'
+                        });
+                    }}>
+                    <Featured/>
+                </TabBarIOS.Item>
+                <TabBarIOS.Item
+                    selected={this.state.selectedTab === 'search'}
+                    title='search'
+                    onPress={() => {
+                        this.setState({
+                            selectedTab: 'search'
+                        });
+                    }}>
+                    <Search/>
+                </TabBarIOS.Item>
+            </TabBarIOS>
+        );
+    }
+}
+ 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
